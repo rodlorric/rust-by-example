7e74694018104c4a9418bd1194e1e3d9001977b3
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2019-05-20 18:58:18 +0100

    Print Hello Worldgit add ., I'm a Rustaceangit add .

---
+++ hello.rs
@@ -0,0 +1,9 @@
+// This is a comment, and will be ignored by the compiler
+// This is the main function
+fn main(){
+    // The statements here will be executed when the compiled binary is called.
+    
+    // Print text to the console.
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
