53a17e34d3cfd5c053a1ac178c04a08c47032070
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2019-05-20 19:07:22 +0100

    Ignore vim files

---
+++ comments.rs
@@ -0,0 +1,17 @@
+fn main(){
+    // This is an example of a line comment
+    // println!("Hello, world!");
+    
+
+    /*
+     * This is another type of comment, the block comment.
+     * Line comments are recommended over the block comments.
+     */
+
+    /*
+    Note, there is no actual need to add the `*` column
+    */
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
