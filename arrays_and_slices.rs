7c8901541cfbe2310f750b6d6ec3c1a2ffff0f2b
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2019-05-21 20:47:33 +0100

    Add Arrays and Slices

--- arrays_and_slices.rs
+++ arrays_and_slices.rs
@@ -1,4 +1,4 @@
-use std:mem;
+use std::mem;
 
 // This function borrows a slice.
 fn analyze_slice(slice: &[i32]) {
@@ -9,3 +9,28 @@ fn analyze_slice(slice: &[i32]) {
 fn main() {
 	// Fixed-size array (type signature is superfluos)
 	let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // All elements can be initialized to the same value
+    let ys: [i32; 500] = [0; 500];
+
+    // Indexing starts at 0
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+
+    // `len` returns the size of the array
+    println!("array size: {}", xs.len());
+
+    // Arrays are stack allocated
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    // Arrays can be automatically borrowed as slice
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    // Slices can point to a section of an array
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1 .. 4]);
+
+    // Out of bound indexing causes compile error
+    // println!("{}", xs[5]);
+}
